eec296d7800ea94a1654038960864c3467f20e87
Stv <dan.steve2910@gmail.com>
2018-05-14 15:00:51 +0700

    tambah table db

---
+++ database/migrations/2018_05_14_075701_create_result_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateResultTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('result', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('commentpath');
+            $table->string('scpath');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('result');
+    }
+}
