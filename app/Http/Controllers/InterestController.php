cf23123c1e010b7dac3f89b0249a89e0ffb50cb7
Jonathan <jonathan16@mhs.if.its.ac.id>
2018-05-28 16:09:38 +0700

    add interest table and model and resource

---
+++ app/Http/Controllers/InterestController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\interest;
+use Illuminate\Http\Request;
+
+class InterestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\interest  $interest
+     * @return \Illuminate\Http\Response
+     */
+    public function show(interest $interest)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\interest  $interest
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(interest $interest)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\interest  $interest
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, interest $interest)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\interest  $interest
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(interest $interest)
+    {
+        //
+    }
+}
---
+++ database/migrations/2018_05_28_090525_create_interests_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInterestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('interests', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('file_path');
+            $table->unsignedInteger('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('interests');
+    }
+}
